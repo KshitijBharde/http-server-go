c07bb51f9fcae6b42ffa8cfb19fb316fc7ab013a
Kshitij <kshitijbharde02@gmail.com>
2025-09-17 13:47:01 +0530

    checking url and returing resp accordingly

--- app/main.go
+++ app/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 // Ensures gofmt doesn't remove the "net" and "os" imports above (feel free to remove this!)
@@ -27,13 +28,42 @@ func main() {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-
 	defer conn.Close()
 
-	response := "HTTP/1.1 200 OK\r\n\r\n"
-	_, err = conn.Write([]byte(response))
+	buf := make([]byte, 4096) // 4kb buffer
+	n, err := conn.Read(buf)
 
 	if err != nil {
-		fmt.Println("Error writing response:", err.Error())
+		fmt.Println("Conn read error:", err)
+		return
+	}
+
+	rawRequest := string(buf[:n])
+
+	lines := strings.Split(rawRequest, "\r\n")
+
+	if len(lines) > 0 {
+		requestLine := lines[0]
+		parts := strings.Split(requestLine, " ")
+		if len(parts) >= 2 {
+			method := parts[0]
+			path := parts[1]
+
+			if strings.ToLower(method) == "get" && path == "/" {
+				okResponse := "HTTP/1.1 200 OK\r\n\r\n"
+				_, err = conn.Write([]byte(okResponse))
+				if err != nil {
+					fmt.Println("Error writing OK response:", err.Error())
+				}
+				return
+			}
+		}
+	}
+
+	notFoundResponse := "HTTP/1.1 404 Not Found\r\n\r\n"
+	_, err = conn.Write([]byte(notFoundResponse))
+
+	if err != nil {
+		fmt.Println("Error writing Not Found response:", err.Error())
 	}
 }
